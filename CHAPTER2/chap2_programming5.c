a9eef6e7a070b2672a8eb2d2d97f1f6bdc0cb666
yevin <kimpika8541@naver.com>
2021-12-27 00:06:45 +0900

    Upload Solutions for Chapter2 Programming Problems

---
+++ CHAPTER2/chap2_programming5.c
@@ -0,0 +1,9 @@
+/* 쉽게 풀어쓴 C언어 Express 개정3판
+   CHAPTER2 Programming 문제 5번  */
+#include <stdio.h>
+int main() {
+	printf("7+8=%d\n", 7 + 8);
+	printf("7-8=%d\n", 7 - 8);
+	printf("7*8=%d\n", 7 * 8);
+	printf("7/8=%d", 7 / 8);
+}
